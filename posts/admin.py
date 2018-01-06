f536a188e00180817919ead3f6dd6e3916f760f9
accro-zeng <zaiyouxian@gmail.com>
2018-01-06 14:15:44 +0800

    ordering posts by time in admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import Category, Post
 
-# Register your models here.
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'created_time', 'modified_time']
+
+    def get_ordering(self, request):
+        return ['-created_time']
 
 
 admin.site.register(Post, PostAdmin)
